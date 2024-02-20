aec2c0326da4c23af019021b39d8bffd9317180e
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-20 14:27:20 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,20 @@
 
 /*
  * File: 4-puts.c
- * Auth: Brennan D Baraban <9079@holbertonschool.com>
+ * Auth: Sevil Mirzayeva <9079@holbertonschool.com>
  */
 
 #include <stdio.h>
 int main(void)
 {
+
+  /**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+  
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
