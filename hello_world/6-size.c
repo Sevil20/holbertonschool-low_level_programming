c705707164fe4f7754b29c467e630fab39679f83
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-20 15:15:38 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 /*
  * File: 6-size.c
- * Auth: Sevil Mirzayeva <9079@holbertonschool.com>
+ * Auth: Sevil Mirzayeva
  */
 
 #include <stdio.h>
@@ -8,18 +8,15 @@ int main(void)
 {
 
   /**
- * main - Prints "Size of a char: 1 byte(s)
- *                Size of an int: 4 byte(s)
- *                Size of a long int: 4 byte(s)
- *                Size of a long long int: 8 byte(s)
- *                Size of a float: 4 byte(s)
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
  * Return: Always 0.
  */
-  printf("Size of a char: byte(s)\n", sizeof(char)");
-  printf("Size of a int: byte(s)\n", sizeof(int)");
-  printf("Size of a long int: byte(s)\n", sizeof(long int)");
-  printf("Size of a long long int: byte(s)\n", sizeof(long long int)");
-  printf("Size of a float: byte(s)\n", sizeof(float)");
+  printf("Size of a char: %zu byte(s)\n", sizeof(char));
+  printf("Size of a int: %zu byte(s)\n", sizeof(int));
+  printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+  printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+  printf("Size of a float: %zu byte(s)\n", sizeof(float));
   return (0);
 }
