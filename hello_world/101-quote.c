a5a56d3a8cff85fdc8b94817d7906b25e4830014
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-22 00:41:58 +0000

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * File: 6-size.c
+ * Auth: Sevil Mirzayeva
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the and that piece of art is useful" - Dora Ko *                                           rpar, 2015-10-19
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful"" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
