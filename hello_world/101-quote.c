c5d67e73c8ec5e69366a0df5a3293d1a2d36c98a
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-22 09:04:25 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,11 +2,15 @@
 /**
  * main - Entry point for function
  *
+ * Description: Places our quote to stderr
+ * instead of stdout
+ *
  * Return: 1 always (success)
  */
 int main(void)
 {
-        char *word = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *word = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, word, 59);
 	return (1);
 }
