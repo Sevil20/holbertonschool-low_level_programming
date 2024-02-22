085b255f03dde96e87cc6196bcd2d4130315dc97
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-22 08:44:00 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,7 +11,7 @@
  */
 int main(void)
 {
-        char *word = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+        char *word = "and that piece of art is useful\" - Dora         Korpar, 2015-10-19";
 
 
 	write(2, word, 59);
