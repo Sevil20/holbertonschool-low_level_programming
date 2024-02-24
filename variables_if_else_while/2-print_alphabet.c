d8a4e358ace120b7a124e2ffcc271ab49940c78d
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-24 09:33:19 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints all letters of alphabet in lowercase 
- *
- * Return: Always (Success)
- */
+* main - Prints all letters of alphabet in lowercase 
+*
+* Return: Always (Success)
+*/
 int main(void)
 {
       char sl;
       
-      for(sl = 'a'; sl <= 'z'; sl++)
+      for(sl='a';sl<='z';sl++)
       {
 	putchar(sl);
       }
