1e4d1b04d10b5fc7f9a4e7a670e31d875c67b006
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-25 13:34:50 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 /**
-* main - Prints all letters of alphabet in lowercase 
-*
-* Return: Always (Success)
-*/
+ * main - Prints all letters of alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-      char sl;
-      char ul;
-      
-      for(sl='a';sl<='z';sl++)
-      {
-	putchar(sl);
-      }
-      for(ul='A';ul<='Z';ul++)
-      {
-        putchar(ul);
-      }
-      
-      putchar('\n');
-    
-      return(0);
-      
-}  
+	char sl;
+	char ul;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		putchar(sl);
+	}
+	for (ul = 'A'; ul <= 'Z'; ul++)
+	{
+		putchar(ul);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
