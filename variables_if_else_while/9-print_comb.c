4fd1b54aba409dc00a3c4a8390ca54534bae52a8
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-25 15:09:01 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ * Numbers must be separated by ", ", followed by a space
+ * Numbers should be printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
+		}
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
