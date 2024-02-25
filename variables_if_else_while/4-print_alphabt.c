8b6465819de1cb45d2db4b4fd3a607a08435be2d
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-25 13:39:24 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all letters of alphabet in lowercase if conditions are met
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sl;
+
+	if (sl == 'e' && sl == 'q')
+	{
+		for (sl = 'a'; sl <= 'z'; sl++)
+		{
+			putchar(sl);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
