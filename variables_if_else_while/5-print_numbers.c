0d9c179c704db111cef844407073c85fa2e75996
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-25 13:55:45 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all letters of alphabet in lowercase if conditions are met
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+	       printf("%d", a);
+	       a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
