8fc07def63a30eab06a726e776c003ec070aebea
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-27 10:07:16 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_diagonal(int n)
+{
+    int i, j;
+
+    if (n <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            _putchar(' ');
+        }
+        _putchar('\\');
+        _putchar('\n');
+    }
+}
