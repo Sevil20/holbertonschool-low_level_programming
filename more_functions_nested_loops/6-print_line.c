751cc7328c0095b299dace8c383dc0d7cd7eba7d
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-27 08:09:20 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints single digit numbers of base 10, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+    int i;
+
+    if (n > 0 && n != 0)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            putchar('_');
+        }
+    }
+    else
+    {
+        putchar('\n');
+    }
+}
