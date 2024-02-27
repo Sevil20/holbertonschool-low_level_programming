c8ed39bb5d5705dcc14f790b36fdef175f47db42
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-27 07:04:24 +0000

    added new files

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _isupper - Checks if a character is uppercase.
+* @c: The character to be checked.
+*
+* Return: 1 if the character is uppercase, 0 otherwise.
+*/
 int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
