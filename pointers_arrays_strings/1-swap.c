19d948f1d9dad97da35c0c929154c4c943eff002
Sevil20 <sevilmirzyeva010@gmail.com>
2024-02-29 09:44:19 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * 1-swap.c - Takes a pointer to an int as parameter and
+ *               updates the value it points to to 98
+ *
+ * @n: Pointer to an integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 402;
+}
